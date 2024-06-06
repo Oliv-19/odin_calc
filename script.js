c991a834f02791fae79b2a7e5cdab9a376178b6f
Oliv-19 <betza.bruguera@gmail.com>
2024-06-06 09:22:01 -0400

    Add basic functions

---
+++ script.js
@@ -0,0 +1,22 @@
+let operator 
+let num1 
+let num2 
+
+
+
+function add(num1, num2){
+    return Number(num1 + num2)
+}
+function subtract(num1, num2){
+    return Number(num1 - num2)
+}
+function multiply(num1, num2){
+    return Number(num1 * num2)
+}
+function divide(num1, num2){
+    return Number(num1 / num2)
+}
+
+function operate(operator, num1, num2){
+
+}
